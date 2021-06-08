ec2c3d4fefa455fbf9957f0aa6b9069bfe9f6ea6
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-06-08 23:40:04 +0545

    Pretty much get settings

--- assets/js/admin/block.js
+++ assets/js/admin/block.js
@@ -5,24 +5,53 @@ const { ServerSideRender } = wp.components;
 
 registerBlockType( 'music-player/music-player', {
     title: __( 'Music Player', 'music-player' ),
+    description: __( 'The music player by default plays all the music found in your media library. Please upload media files to your media library', 'music-player' ),
     icon: 'format-audio',
-    category: 'common',
+    category: 'widgets',
     attributes: {
-        images : {
-            default: [],
-            type:   'array',
+        theme: {
+            type: 'string',
+        },
+    example: {
+        attributes: {
+            preview: true,
+        },
+    },
+    edit( props ) {
+        const {
+                attributes: {
+                    theme = "default",
+                },
 
+            setAttributes
+         } = props;
+
+        let jsx;
+        
+        function selectTheme( value ) {
+            setAttributes( { theme: value } );
         }
+
+        jsx = [
+            <InspectorControls key="music-player-theme-selector-inspector-controls">
+                <PanelBody title="Music Player Settings">
+                    <SelectControl
+                        label= "Select Theme"
+                        value= { theme }
+                        options={[
+                                { value: 'default', label: 'Default' },
+                                { value: 'circular-pink', label: 'Circular Pink' }
+                            ]}    
+                        onChange={ selectTheme }
+                    />
+                </PanelBody>
+            </InspectorControls>
+        ];
+
+        return jsx;
     },
-    edit: function ({ attributes }) {
-        return (
-            <ServerSideRender
-                block="music-player/music-player"
-            />
-        );
-    },
-    save( { attributes, className } ) {
+    save() {
         // Gutenberg will save attributes we can use in server-side callback
        return null;
-    },
-} );
+    }
+});
