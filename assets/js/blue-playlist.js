b9f40042cc0b771fa63f7060e75e9f1d67ab7662
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-06-15 17:25:46 +0545

    Load flatblack and blueplaylsit on block

--- assets/js/blue-playlist.js
+++ assets/js/blue-playlist.js
@@ -1,66 +1,96 @@
-
-/*
-	When the bandcamp link is pressed, stop all propagation so AmplitudeJS doesn't
-	play the song.
-*/
-let bandcampLinks = document.getElementsByClassName('bandcamp-link');
-
-for( var i = 0; i < bandcampLinks.length; i++ ){
-	bandcampLinks[i].addEventListener('click', function(e){
-		e.stopPropagation();
-	});
-}
-
-
-let songElements = document.getElementsByClassName('song');
-
-for( var i = 0; i < songElements.length; i++ ){
-	/*
-		Ensure that on mouseover, CSS styles don't get messed up for active songs.
-	*/
-	songElements[i].addEventListener('mouseover', function(){
-		this.style.backgroundColor = '#00A0FF';
-
-		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#FFFFFF';
-		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#FFFFFF';
-
-		if( !this.classList.contains('amplitude-active-song-container') ){
-			this.querySelectorAll('.play-button-container')[0].style.display = 'block';
-		}
-
-		this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'none';
-		this.querySelectorAll('img.bandcamp-white')[0].style.display = 'block';
-		this.querySelectorAll('.song-duration')[0].style.color = '#FFFFFF';
-	});
-
-	/*
-		Ensure that on mouseout, CSS styles don't get messed up for active songs.
-	*/
-	songElements[i].addEventListener('mouseout', function(){
-		this.style.backgroundColor = '#FFFFFF';
-		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#272726';
-		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#607D8B';
-		this.querySelectorAll('.play-button-container')[0].style.display = 'none';
-		this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'block';
-		this.querySelectorAll('img.bandcamp-white')[0].style.display = 'none';
-		this.querySelectorAll('.song-duration')[0].style.color = '#607D8B';
-	});
-
-	/*
-		Show and hide the play button container on the song when the song is clicked.
-	*/
-	songElements[i].addEventListener('click', function(){
-		this.querySelectorAll('.play-button-container')[0].style.display = 'none';
-	});
-}
-
-Amplitude.init({
-  "bindings": {
-    37: 'prev',
-    39: 'next',
-    32: 'play_pause'
-  },
-  "songs": audio_files
+// Trigger things on frontend.
+jQuery( function ($) {
+	bluePlaylistInit();
 });
+
+// Trigger things on block rendering.
+document.addEventListener(
+	'musicPlayerInitialize',
+	function( e ) {
+		window.setTimeout( bluePlaylistInit, 20 );
+	},
+	false
+);
+
+let blockLoadedForBluePlaylist = false;
+let blockLoadedForBluePlaylistInterval = setInterval(function() {
+
+    if (document.getElementById('post-title-1')) {/*post-title-1 is ID of Post Title Textarea*/
+
+		bluePlaylistInit();
+        blockLoadedForBluePlaylist = true;
+    }
+    if ( blockLoadedForBluePlaylist ) {
+        clearInterval( blockLoadedForBluePlaylistInterval );
+    }
+}, 20 );
+
+
+const bluePlaylistInit = function() {
+
+	/*
+		When the bandcamp link is pressed, stop all propagation so AmplitudeJS doesn't
+		play the song.
+	*/
+	let bandcampLinks = document.getElementsByClassName('bandcamp-link');
+
+	for( var i = 0; i < bandcampLinks.length; i++ ){
+		bandcampLinks[i].addEventListener('click', function(e){
+			e.stopPropagation();
+		});
+	}
+
+
+	let songElements = document.getElementsByClassName('song');
+
+	for( var i = 0; i < songElements.length; i++ ){
+		/*
+			Ensure that on mouseover, CSS styles don't get messed up for active songs.
+		*/
+		songElements[i].addEventListener('mouseover', function(){
+			this.style.backgroundColor = '#00A0FF';
+
+			this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#FFFFFF';
+			this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#FFFFFF';
+
+			if( !this.classList.contains('amplitude-active-song-container') ){
+				this.querySelectorAll('.play-button-container')[0].style.display = 'block';
+			}
+
+			this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'none';
+			this.querySelectorAll('img.bandcamp-white')[0].style.display = 'block';
+			this.querySelectorAll('.song-duration')[0].style.color = '#FFFFFF';
+		});
+
+		/*
+			Ensure that on mouseout, CSS styles don't get messed up for active songs.
+		*/
+		songElements[i].addEventListener('mouseout', function(){
+			this.style.backgroundColor = '#FFFFFF';
+			this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#272726';
+			this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#607D8B';
+			this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+			this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'block';
+			this.querySelectorAll('img.bandcamp-white')[0].style.display = 'none';
+			this.querySelectorAll('.song-duration')[0].style.color = '#607D8B';
+		});
+
+		/*
+			Show and hide the play button container on the song when the song is clicked.
+		*/
+		songElements[i].addEventListener('click', function(){
+			this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+		});
+	}
+
+	Amplitude.init({
+	  "bindings": {
+	    37: 'prev',
+	    39: 'next',
+	    32: 'play_pause'
+	  },
+	  "songs": audio_files
+	});
+}
 
 document.getElementById('large-visualization').style.height = document.getElementById('album-art').offsetWidth + 'px';
--- assets/js/flat-black.js
+++ assets/js/flat-black.js
@@ -1,46 +1,75 @@
-window.onkeydown = function(e) {
-    return !(e.keyCode == 32);
-};
-
-/*
-  Handles a click on the down button to slide down the playlist.
-*/
-document.getElementsByClassName('down-header')[0].addEventListener('click', function(){
-  var list = document.getElementById('list');
-
-  list.style.height = ( parseInt( document.getElementById('flat-black-player-container').offsetHeight ) - 135 ) + 'px';
-
-  document.getElementById('list-screen').classList.remove('slide-out-top');
-  document.getElementById('list-screen').classList.add('slide-in-top');
-  document.getElementById('list-screen').style.display = "block";
+// Trigger things on frontend.
+jQuery( function ($) {
+	FlatBlackInit();
 });
 
-/*
-  Handles a click on the up arrow to hide the list screen.
-*/
-document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function(){
-  document.getElementById('list-screen').classList.remove('slide-in-top');
-  document.getElementById('list-screen').classList.add('slide-out-top');
-  document.getElementById('list-screen').style.display = "none";
-});
+// Trigger things on block rendering.
+document.addEventListener(
+	'musicPlayerInitialize',
+	function( e ) {
+		window.setTimeout( FlatBlackInit, 20 );
+	},
+	false
+);
 
-/*
-  Handles a click on the song played progress bar.
-*/
-document.getElementById('song-played-progress').addEventListener('click', function( e ){
-  var offset = this.getBoundingClientRect();
-  var x = e.pageX - offset.left;
+let blockLoadedForFlatBlack = false;
+let blockLoadedForFlatBlackInterval = setInterval(function() {
 
-  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
-});
+    if (document.getElementById('post-title-1')) {/*post-title-1 is ID of Post Title Textarea*/
 
-document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+		FlatBlackInit();
+        blockLoadedForFlatBlack = true;
+    }
+    if ( blockLoadedForFlatBlack ) {
+        clearInterval( blockLoadedForAPlayerInterval );
+    }
+}, 20 );
 
-Amplitude.init({
-  "bindings": {
-    37: 'prev',
-    39: 'next',
-    32: 'play_pause'
-  },
-  "songs": audio_files
-});
+const FlatBlackInit = function() {
+	window.onkeydown = function(e) {
+	    return !(e.keyCode == 32);
+	};
+
+	/*
+	  Handles a click on the down button to slide down the playlist.
+	*/
+	document.getElementsByClassName('down-header')[0].addEventListener('click', function(){
+	  var list = document.getElementById('list');
+
+	  list.style.height = ( parseInt( document.getElementById('flat-black-player-container').offsetHeight ) - 135 ) + 'px';
+
+	  document.getElementById('list-screen').classList.remove('slide-out-top');
+	  document.getElementById('list-screen').classList.add('slide-in-top');
+	  document.getElementById('list-screen').style.display = "block";
+	});
+
+	/*
+	  Handles a click on the up arrow to hide the list screen.
+	*/
+	document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function(){
+	  document.getElementById('list-screen').classList.remove('slide-in-top');
+	  document.getElementById('list-screen').classList.add('slide-out-top');
+	  document.getElementById('list-screen').style.display = "none";
+	});
+
+	/*
+	  Handles a click on the song played progress bar.
+	*/
+	document.getElementById('song-played-progress').addEventListener('click', function( e ){
+	  var offset = this.getBoundingClientRect();
+	  var x = e.pageX - offset.left;
+
+	  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+	});
+
+	document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+
+	Amplitude.init({
+	  "bindings": {
+	    37: 'prev',
+	    39: 'next',
+	    32: 'play_pause'
+	  },
+	  "songs": audio_files
+	});
+}
